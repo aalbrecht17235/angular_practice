15d17e97c8497cfd2c74aa8a5b1464df7405e4e0
Andrew Albrecht <andrewalbrecht@utexas.edu>
2019-05-20 21:46:28 -0500

    functional todo list

--- angular-crash-todolist/src/app/components/todos/todos.component.ts
+++ angular-crash-todolist/src/app/components/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../../services/todo.service'
 import { Todo } from '../../models/Todo';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -18,6 +19,20 @@ export class TodosComponent implements OnInit {
     this.todoService.getTodos().subscribe( todos => {
       this.todos = todos;
     });
+  }
+
+
+  deleteTodo(todo:Todo) {
+    // Remove from UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    // Remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    })
   }
 
 }
